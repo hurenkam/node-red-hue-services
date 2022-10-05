6e8639e337e7d964b028a84749dbd708a98e20d0
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-10-05 02:43:13 +0200

    Added status

--- mh-hue-light.js
+++ mh-hue-light.js
@@ -10,8 +10,24 @@ module.exports = function(RED) {
         this.bridge = RED.nodes.getNode(config.bridge);
         this.url = "/clip/v2/resource/light/" + this.light;
 
+        this.state = { on: false, brightness: 0 };
+	
         this.onUpdate = function(resource) {
             node.send({ payload: resource});
+
+            if (resource.on) {
+                node.state.on = resource.on.on;
+            }
+
+            if (resource.dimming) {
+                node.state.brightness = resource.dimming.brightness;
+            }
+
+            if (node.state.on === true) {
+                node.status({fill: "yellow", shape: "dot", text: "on (" + node.state.brightness + "%)"});
+            } else {
+                node.status({fill: "grey", shape: "dot", text: "off"});
+            }
         }
         this.bridge.subscribe(this.light,this.onUpdate);
 
