1f6fe07bc85d85e753a6afebd4e9622b0dfaf3c9
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-10-20 17:34:20 +0200

    Send events to proper output in case of multiple outputs. Deliver input msg to correct service(s)

--- mh-hue-device.js
+++ mh-hue-device.js
@@ -17,6 +17,11 @@ module.exports = function(RED) {
                 var index = node.services.indexOf(resource.id);
                 var msg = [{ index: index, payload: resource }];
 
+                if (node.multi) {
+                    for (let i = 0; i < index; i++) {
+                        msg.unshift(null);
+                    }
+                }
                 node.send(msg);
             }
         }
@@ -31,6 +36,41 @@ module.exports = function(RED) {
             });
         }, 5000);
     }
+
+    this.on('input', function(msg) {
+
+        if (msg.rtypes) {
+            // if msg contains a list of rtypes
+            // then forward the msg to all services that have a matching rtype
+            for (const [key, value] of Object.entries(node.services)) {
+                const url = "/clip/v2/resource/" + key + "/" + value;
+                if (msg.rtypes.includes(key)) {
+                    node.bridge.put(url,msg.payload);
+                }
+            }
+        }
+
+        if (msg.rids) {
+            // if msg contains a list of rids
+            // then forward the msg to all services that have a matching rid
+            for (const [key, value] of Object.entries(node.services)) {
+                const url = "/clip/v2/resource/" + key + "/" + value;
+                if (msg.rids.includes(value)) {
+                    node.bridge.put(url,msg.payload);
+                }
+            }
+        }
+
+        if (!(msg.rids) && !(msg.rtypes))
+        {
+            // if msg does not contain a list of rids or rtypes
+            // then assume it is meant for all services registered with htis node
+            for (const [key, value] of Object.entries(node.services)) {
+                const url = "/clip/v2/resource/" + key + "/" + value;
+                node.bridge.put(url,msg.payload);
+            }
+        }
+    });
 
     RED.nodes.registerType("mh-hue-device",HueDevice);
 }
