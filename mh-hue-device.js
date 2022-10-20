5cedc5bd585e62da089460dcf6643df759ec835b
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-10-20 17:36:53 +0200

    Bugfix

--- mh-hue-device.js
+++ mh-hue-device.js
@@ -35,42 +35,42 @@ module.exports = function(RED) {
                 });
             });
         }, 5000);
+
+        this.on('input', function(msg) {
+
+            if (msg.rtypes) {
+                // if msg contains a list of rtypes
+                // then forward the msg to all services that have a matching rtype
+                for (const [key, value] of Object.entries(node.services)) {
+                    const url = "/clip/v2/resource/" + key + "/" + value;
+                    if (msg.rtypes.includes(key)) {
+                        node.bridge.put(url,msg.payload);
+                    }
+                }
+            }
+
+            if (msg.rids) {
+                // if msg contains a list of rids
+                // then forward the msg to all services that have a matching rid
+                for (const [key, value] of Object.entries(node.services)) {
+                    const url = "/clip/v2/resource/" + key + "/" + value;
+                    if (msg.rids.includes(value)) {
+                        node.bridge.put(url,msg.payload);
+                    }
+                }
+            }
+
+            if (!(msg.rids) && !(msg.rtypes))
+            {
+                // if msg does not contain a list of rids or rtypes
+                // then assume it is meant for all services registered with htis node
+                for (const [key, value] of Object.entries(node.services)) {
+                    const url = "/clip/v2/resource/" + key + "/" + value;
+                    node.bridge.put(url,msg.payload);
+                }
+            }
+        });
     }
-
-    this.on('input', function(msg) {
-
-        if (msg.rtypes) {
-            // if msg contains a list of rtypes
-            // then forward the msg to all services that have a matching rtype
-            for (const [key, value] of Object.entries(node.services)) {
-                const url = "/clip/v2/resource/" + key + "/" + value;
-                if (msg.rtypes.includes(key)) {
-                    node.bridge.put(url,msg.payload);
-                }
-            }
-        }
-
-        if (msg.rids) {
-            // if msg contains a list of rids
-            // then forward the msg to all services that have a matching rid
-            for (const [key, value] of Object.entries(node.services)) {
-                const url = "/clip/v2/resource/" + key + "/" + value;
-                if (msg.rids.includes(value)) {
-                    node.bridge.put(url,msg.payload);
-                }
-            }
-        }
-
-        if (!(msg.rids) && !(msg.rtypes))
-        {
-            // if msg does not contain a list of rids or rtypes
-            // then assume it is meant for all services registered with htis node
-            for (const [key, value] of Object.entries(node.services)) {
-                const url = "/clip/v2/resource/" + key + "/" + value;
-                node.bridge.put(url,msg.payload);
-            }
-        }
-    });
 
     RED.nodes.registerType("mh-hue-device",HueDevice);
 }
