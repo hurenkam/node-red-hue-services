3ab3e2aa868f3d911ea2609305f9e09c754aa22c
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-10-22 02:30:56 +0200

    Add WallSwitchModule class

---
+++ src/WallSwitchModule.js
@@ -0,0 +1,39 @@
+const Base = require('./Base');
+
+class WallSwitchModule extends Base {
+    constructor(node,config,bridge) {
+        super(node,config,bridge);
+        console.log("WallSwitchModule[" + this.name + "].constructor()");
+
+        this.button = null;
+
+        console.log(this);
+    }
+
+    onUpdate(resource) {
+        super.onUpdate(resource);
+        console.log("WallSwitchModule["+this.name+"].onUpdate()");
+
+        if (resource.type === "button") {
+            this.button = resource.button;
+            setTimeout(() => { 
+                this.button = null;
+                this.updateStatus();
+            },10000);
+        }
+
+        this.updateStatus();
+    }
+
+    getStatusText() {
+        var text = super.getStatusText();
+
+        if (this.button!=null) {
+            text = this.button.last_event;
+        }
+
+        return text;
+    }
+}
+
+module.exports = WallSwitchModule;
