2f2504a2f91f9f70bc5be943fc0c96e265a72f45
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-10-23 15:36:35 +0200

    Refactor BridgeConfigNode to be a modern class

--- nodes/BridgeConfigNode.js
+++ nodes/BridgeConfigNode.js
@@ -5,30 +5,39 @@ module.exports = function (RED) {
 
     const ClipApi = require('../src/ClipApi');
 
-    function BridgeConfigNode(config) {
-        RED.nodes.createNode(this, config);
-        this._config = config;
-        console.log("BridgeConfigNode[" + config.name + "].constructor()")
-        console.log(config);
+    class BridgeConfigNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            this._config = config;
+            console.log("BridgeConfigNode[" + config.name + "].constructor()")
+            console.log(config);
+    
+            this.clip = new ClipApi(config.name, config.ip, config.key);
+            bridges[this.id] = { id: this.id, name: config.name, instance: this };
+    
+            this.on('close', function () {
+                console.log("BridgeConfigNode[" + this._config.name + "].on('close')");
+                this.clip.destructor();
+                this.clip = null;
+            });
+        }
 
-        this.clip = new ClipApi(config.name, config.ip, config.key);
-        bridges[this.id] = { id: this.id, name: config.name, instance: this };
-
-        this.on('close', function () {
-            console.log("BridgeConfigNode[" + this._config.name + "].on('close')");
-            this.clip.destructor();
-            this.clip = null;
-        });
+        getHueApplicationKey() {
+            console.log("BridgeConfigNode[" + this._config.name + "].getHueApplicationKey()");
+        }
     }
 
     RED.nodes.registerType("BridgeConfigNode", BridgeConfigNode);
 
-    RED.httpAdmin.get('/BridgeConfigNode/GetDiscoveredBridges', async function (req, res, next) {
-        console.log("BridgeConfigNode.get(\"/BridgeConfigNode/GetDiscoveredBridges()\")");
+    RED.httpAdmin.get('/BridgeConfigNode/DiscoverBridges', async function (req, res, next) {
+        console.log("BridgeConfigNode.get(\"/BridgeConfigNode/DiscoverBridges()\")");
     });
 
     RED.httpAdmin.get('/BridgeConfigNode/GetHueApplicationKey', async function (req, res, next) {
         console.log("BridgeConfigNode.get(\"/BridgeConfigNode/GetHueApplicationKey()\")");
+        var bridge = bridges[req.query.bridge_id].instance;
+        var key = bridge.getHueApplicationKey();
+        res.end(JSON.stringigy(Object(key)));
     });
 
     RED.httpAdmin.get('/BridgeConfigNode/GetSortedDeviceServices', async function (req, res, next) {
