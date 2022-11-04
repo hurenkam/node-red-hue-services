75a53308b709a8ed60c2aec83f6034c030fe2b1a
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-11-04 20:31:24 +0100

    Refactor Scene UI

---
+++ resources/SceneUI.js
@@ -0,0 +1,14 @@
+class SceneUI extends DeviceUI {
+    constructor() {
+        super("Scene");
+        console.log("SceneUI.constructor()");
+
+        this.config.color = "#FFAAAA";
+        this.config.inputs = 1;
+        this.config.outputs = 1;
+        this.config.icon = "light.svg";
+
+        this.rtype = "scene";
+        this.models = null;
+    }
+}
