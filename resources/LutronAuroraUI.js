00b894447ce40976b6e68f9b474ed921933382d4
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-11-04 18:41:28 +0100

    Refactor Lutran Aurora UI

---
+++ resources/LutronAuroraUI.js
@@ -0,0 +1,8 @@
+class LutronAuroraUI extends DeviceUI {
+    constructor() {
+        super("Lutron Aurora Dimmer Switch");
+        console.log("LutronAuroraUI.constructor()");
+
+        this.models = ["Z3-1BRL"];
+    }
+}
