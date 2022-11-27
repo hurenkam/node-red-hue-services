c7fcbf1df0f04f5c43a7af144efde8699c217806
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-11-27 12:27:37 +0100

    Refactor ui code

--- src/ui/BridgeConfigUI.js
+++ src/ui/BridgeConfigUI.js
@@ -10,17 +10,10 @@ export class BridgeConfigUI extends BaseUI {
         this.config.defaults.key = { value:"", required: true }
     }
 
-    build(config) {
-        super.build(config);
-        console.log("BridgeConfigUI.build()");
-
-        var template_root = this.getTemplateRoot();
-        if (!template_root) {
-            console.log("template-root not found.")
-            return;
-        }
-
-        this.selectInput(template_root,"ip","IP",config.ip);
-        this.selectInput(template_root,"key","Key",config.key);
+    ui() {
+        var text = super.ui();
+        text += this.uiSelectInput("ip","IP");
+        text += this.uiSelectInput("key","Key");
+        return text;
     }
 }
