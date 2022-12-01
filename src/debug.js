a8d2f5df0808a177abab1df28be30e2bea9a418e
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-12-01 19:09:23 +0100

    Add comments explaining how to manipulate debug logging levels

--- src/debug.js
+++ src/debug.js
@@ -1,8 +1,19 @@
+// ====================
+// Manage debug logging
+// ====================
+
 module.exports = function (RED) {
 
     const debug = require('debug');
 
-    // Manage debug logging
+    // GET: Retrieves current setting, but beware:
+    //      There is a bug in the debug module where it not
+    //      always accepts the same namespaces as input
+    //      causing the GET sometimes to error and leave
+    //      debugging disabled alltogether.
+    //
+    //      Use the following command to retrieve the settings:
+    //      curl -i -H "Accept: application/json" 'localhost:1880/debug'
     RED.httpAdmin.get('/debug', function (req, res) {
         console.log("/debug",req.method);
         var namespaces = debug.disable();
@@ -11,6 +22,10 @@ module.exports = function (RED) {
         res.end(JSON.stringify(Object({ "namespaces": namespaces })));
     });
 
+    // POST: Using POST you can set the debug level to a new
+    //       namespaces setting.
+    //       e.g. use the following command to enable error and warning logging:
+    //       curl -i -H "Accept: application/json" 'localhost:1880/debug' -d "namespaces='error:*,warn:*'"
     RED.httpAdmin.post('/debug', function (req, res) {
         console.log("/debug",req.method,req.body);
 
@@ -28,6 +43,9 @@ module.exports = function (RED) {
         res.end(JSON.stringify(Object({ "error": "Only GET and PUT are supported." })));
     });
 
+    // DELETE: Using DELETE you can disable the debug logging.
+    //         e.g. use the following command:
+    //         curl -i -H "Accept: application/json" 'localhost:1880/debug' -X "DELETE"
     RED.httpAdmin.delete('/debug', function (req, res) {
         console.log("/debug",req.method);
         var namespaces = debug.disable();
