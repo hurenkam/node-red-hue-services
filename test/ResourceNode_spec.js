757652a11477c967b3e59498ef84e1e72188caee
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-12-05 22:01:54 +0100

    Some more unit tests for ResourceNode

--- test/ResourceNode_spec.js
+++ test/ResourceNode_spec.js
@@ -2,6 +2,7 @@ var helper = require("node-red-node-test-helper");
 const TestResourceNode = require("./mocks/TestResourceNode");
 const assert = require('assert');
 const { mock } = require("./mocks/TestResourceNode");
+const { TIMEOUT } = require("dns");
 
 var testnodes = function(RED) {
     "use strict";
@@ -224,7 +225,6 @@ describe('Resource Node', function () {
             id: "n1",
             type: "ResourceNode",
             name: "resource node",
-            wires: ["n2"]
         }];
 
         TestResourceNode.mock = {
@@ -268,6 +268,64 @@ describe('Resource Node', function () {
             var n1 = helper.getNode("n1");
             n1.start(new TestResource({ name: "test resource" }));
             n1.receive({ rids: ["1c0fb40c-41af-4ed3-a2e0-552398dbd0d8"], payload: {} });
+        });
+    });
+
+    it('should not call resource.put when onInput(msg) is called and msg.rids contains no matching rid', function (done) {
+        var flow = [{
+            id: "n1",
+            type: "ResourceNode",
+            name: "resource node",
+            uuid: "1c0fb40c-41af-4ed3-a2e0-552398dbd0d8"
+        }];
+
+        TestResourceNode.mock = {
+            bridge: function () {
+                return new TestBridge();
+            }
+        }
+
+        TestResource.mock = {
+            rid: function() { return "1c0fb40c-41af-4ed3-a2e0-552398dbd0d8" },
+            put: function(msg) { assert.fail('did not expect resource.put() to be called'); }
+        }
+
+        helper.load(testnodes, flow, function () {
+            var n1 = helper.getNode("n1");
+            n1.start(new TestResource({ name: "test resource" }));
+            n1.receive({ rids: ["4157244d-f3ba-4fa3-857b-b946b3d661ce"], payload: {} });
+            setTimeout(() => {
+                done();
+            }, 0);
+        });
+    });
+
+    it('should not call resource.put when onInput(msg) is called and msg.rtypes contains no matching type', function (done) {
+        var flow = [{
+            id: "n1",
+            type: "ResourceNode",
+            name: "resource node",
+            uuid: "1c0fb40c-41af-4ed3-a2e0-552398dbd0d8"
+        }];
+
+        TestResourceNode.mock = {
+            bridge: function () {
+                return new TestBridge();
+            }
+        }
+
+        TestResource.mock = {
+            rtype: function() { return "button" },
+            put: function(msg) { assert.fail('did not expect resource.put() to be called'); }
+        }
+
+        helper.load(testnodes, flow, function () {
+            var n1 = helper.getNode("n1");
+            n1.start(new TestResource({ name: "test resource" }));
+            n1.receive({ rtypes: ["light"], payload: {} });
+            setTimeout(() => {
+                done();
+            }, 0);
         });
     });
 });
